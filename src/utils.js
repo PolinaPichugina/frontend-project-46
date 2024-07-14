625cb6f01de76d66cd9ffd40d8c36a25e299e615
Polina Pichugina <p.cipileva@mail.ru>
2024-07-14 18:14:54 +0500

    k

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,10 @@
 import { fileURLToPath } from 'url';
-import {join, dirname } from 'path';
+import {resolve, dirname } from 'path';
 import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixtureFilepath = (filepath) => join(__dirname,  '..', '__fixtures__', filepath);
+const getFixtureFilepath = (filepath) => resolve(__dirname,  '..', '__fixtures__', filepath);
 
 const readFile = (filepath) => readFileSync(getFixtureFilepath(filepath), 'utf-8');
 
