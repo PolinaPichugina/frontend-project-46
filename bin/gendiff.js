6a6ea2814ddfb75f8314c781b268ef67e7d7f901
Polina Pichugina <p.cipileva@mail.ru>
2024-07-14 18:27:41 +0500

    fix replacer

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import {Command} from 'commander';
-import genDiff from '../AST.js'
+import genDiff from '../src/index.js'
 const program = new Command;
 program
   .name('gendiff')
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const replacer = '  ';
+const replacer = '    ';
 
 const bracketIntent = (replacer, depth) => replacer.repeat(depth - 1);
 
@@ -26,9 +26,9 @@ const buildTree = (array, depth) => {
             case 'deleted': return `${ident}- ${line.key}: ${getValue(line.value, depth + 1)}`;
             case 'changed': return [
                 `${ident}- ${line.key}: ${getValue(line.value1, depth + 1)}`,
-                `${ident}+ ${line.key}: ${getValue(line.value, depth + 1)}`
+                `${ident}+ ${line.key}: ${getValue(line.value2, depth + 1)}`
             ];
-            case 'unchanged': return `${ident.repeat(2)}${line.key}: ${getValue(line.value, depth + 1)}`;
+            case 'unchanged': return `${replacer.repeat(2)}${line.key}: ${getValue(line.value, depth + 1)}`;
             case 'nested':  return `${ident}${line.key}: ${buildTree(line.value, depth + 1)}`;
         }
     });
