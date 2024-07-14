30b42089470a3d2e6c80ed134acb85962cc920bb
Polina Pichugina <p.cipileva@mail.ru>
2024-07-14 18:08:29 +0500

    fix test

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ import { readFileSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const getFixtureFilepath = (filepath) => join(__dirname,  '__fixtures__', filepath);
+const getFixtureFilepath = (filepath) => join(__dirname,  '..', '__fixtures__', filepath);
 
 const readFile = (filepath) => readFileSync(getFixtureFilepath(filepath), 'utf-8');
 
