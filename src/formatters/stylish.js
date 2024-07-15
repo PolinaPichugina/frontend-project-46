0083ea831c08fccf822bc9304ec772356f7f047e
Polina Pichugina <p.cipileva@mail.ru>
2024-07-15 14:05:46 +0500

    l

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const replacer = '    ';
 
-const bracketIntent = (replacer, depth) => replacer.repeat(depth - 1);
+const bracketIntent = (replacer, depth) => replacer.repeat(depth);
 
 const getStr = (lines, depth) => ['{', ...lines, `${bracketIntent(replacer, depth)}}`].join('\n');
 
@@ -28,8 +28,8 @@ const buildTree = (array, depth) => {
                 `${ident}- ${line.key}: ${getValue(line.value1, depth + 1)}`,
                 `${ident}+ ${line.key}: ${getValue(line.value2, depth + 1)}`
             ];
-            case 'unchanged': return `${replacer.repeat(2)}${line.key}: ${getValue(line.value, depth + 1)}`;
-            case 'nested':  return `${ident}${line.key}: ${buildTree(line.value, depth + 1)}`;
+            case 'unchanged': return `${ident}  ${line.key}: ${getValue(line.value, depth + 1)}`;
+            case 'nested':  return `${ident}  ${line.key}: ${buildTree(line.value, depth + 1)}`;
         }
     });
     return getStr(result, depth);
